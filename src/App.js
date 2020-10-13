ac3bc8a031518138d871b99897a0b074c9b7d252
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-13 13:28:45 +0200

    Refactored to use Axios, added some CSS

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, {useCallback, useEffect, useReducer, useRef, useState} from 'react';
+import axios from 'axios';
+import './App.css';
 
 const API_ENDPOINT = 'https://hn.algolia.com/api/v1/search?query=';
 
@@ -51,28 +53,28 @@ const App = () => {
 
     const [searchTerm, setSearchTerm] = useSemiPersistentState('search', 'React');
 
+    const [url, setUrl] = useState(
+        `${API_ENDPOINT}${searchTerm}`
+    )
+
     const [stories, dispatchStories] = useReducer(
         storiesReducer,
-        { data: [], isLoading: false, isError: false}
+        {data: [], isLoading: false, isError: false}
     );
 
-    const handleFetchStories = useCallback(() => {
-                if (!searchTerm) return;
-
+    const handleFetchStories = useCallback(async () => {
         dispatchStories({type: 'STORIES_FETCH_INIT'});
 
-        fetch(`${API_ENDPOINT}${searchTerm}`)
-            .then(respose => respose.json())
-            .then(result => {
-                dispatchStories({
-                    type: 'STORIES_FETCH_SUCCESS',
-                    payload: result.hits,
-                });
-            })
-            .catch(() =>
-                dispatchStories({type: 'STORIES_FETCH_FAILURE'})
-            );
-    }, [searchTerm]);
+        try {
+            const result = await axios.get(url)
+            dispatchStories({
+                type: 'STORIES_FETCH_SUCCESS',
+                payload: result.data.hits,
+            });
+        } catch {
+            dispatchStories({type: 'STORIES_FETCH_FAILURE'})
+        }
+    }, [url]);
 
     useEffect(() => {
         handleFetchStories();
@@ -85,23 +87,23 @@ const App = () => {
         });
     };
 
-    const handleSearch = event => {
+    const handleSearchInput = event => {
         setSearchTerm(event.target.value);
+    }
+
+    const handleSearchSubmit = event => {
+        event.preventDefault();
+        setUrl(`${API_ENDPOINT}${searchTerm}`)
     };
 
     return (
-        <div>
-            <h1>My Hacker Stories</h1>
-
-            <InputWithLabel
-                id='search'
-                value={searchTerm}
-                isFocused
-                onInputChange={handleSearch}
-            >
-                <strong>Search:</strong>
-            </InputWithLabel>
-            <hr/>
+        <div className='container'>
+            <h1 className='headline-primary'>My Hacker Stories</h1>
+            <SearchForm
+                searchTerm={searchTerm}
+                onSearchInput={handleSearchInput}
+                onSearchSubmit={handleSearchSubmit}
+            />
             {stories.isError && <p>Something went wrong ...</p>}
             {stories.isLoading ?
                 (<p>Loading ...</p>) : (<List list={stories.data} onRemoveItem={handleRemoveStory}/>)}
@@ -129,6 +131,7 @@ const InputWithLabel = ({id, label, value, type = 'text', onInputChange, isFocus
                 type={type}
                 value={value}
                 onChange={onInputChange}
+                className='input'
             />
         </>
     )
@@ -138,17 +141,37 @@ const List = ({list, onRemoveItem}) =>
     list.map(item => <Item key={item.objectID} item={item} onRemoveItem={onRemoveItem}/>)
 
 const Item = ({item, onRemoveItem}) => (
-    <div>
-        <span>
+    <div className='item'>
+        <span style={{ width: '40%'}}>
             <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%'}}>{item.num_comments}</span>
+        <span style={{ width: '10%'}}>{item.points}</span>
+        <span style={{ width: '10%'}}>
             <button type='button' onClick={() => onRemoveItem(item)}>Dismiss</button>
         </span>
     </div>
 )
+
+const SearchForm = ({searchTerm, onSearchInput, onSearchSubmit}) => (
+    <form onSubmit={onSearchSubmit} className='search-form'>
+        <InputWithLabel
+            id='search'
+            value={searchTerm}
+            isFocused
+            onInputChange={onSearchInput}
+        >
+            <strong>Search:</strong>
+        </InputWithLabel>
+        <button
+            type='submit'
+            disabled={!searchTerm}
+            className='button button_large'
+        >
+            Submit
+        </button>
+    </form>
+);
 
 export default App;
