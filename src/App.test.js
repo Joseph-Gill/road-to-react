3fe9d588f71027d4678c924e9c908286227a22a0
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-15 13:19:49 +0200

    Added Jest tests for SearchForm and List

--- src/App.test.js
+++ src/App.test.js
@@ -36,3 +36,72 @@ describe('Item', () => {
     });
 
 });
+
+describe ('List', () => {
+    const list = [
+        {
+            title: 'React',
+            url: 'https://reactjs.org/',
+            author: 'Jordan Walke',
+            num_comments: 3,
+            points: 4,
+            objectID: 0
+        },
+        {
+            title: 'Redux',
+            url: 'https://redux.js.org/',
+            author: 'Dan Abramov, Andrew Clark',
+            num_comments: 2,
+            points: 5,
+            objectID: 1,
+        },
+    ];
+
+    it('renders two items', () => {
+        const component = renderer. create(<List list={list} />);
+        expect(component.root.findAllByType(Item).length).toEqual(2);
+    });
+});
+
+describe('SearchForm', () => {
+    const searchFormProps = {
+        searchTerm: 'React',
+        onSearchInput: jest.fn(),
+        onSearchSubmit: jest.fn(),
+    };
+
+    let component;
+
+    beforeEach(() => {
+        component = renderer.create(<SearchForm {...searchFormProps}/>);
+    });
+
+    it('renders the input field with its value', () => {
+       const value = component.root.findByType('input').props.value;
+       expect(value).toEqual('React');
+    });
+
+    it('changes the input field', () => {
+        const pseudoEvent = { target: 'Redux' };
+
+        component.root.findByType('input').props.onChange(pseudoEvent);
+
+        expect(searchFormProps.onSearchInput).toHaveBeenCalledTimes(1);
+        expect(searchFormProps.onSearchInput).toHaveBeenCalledWith(pseudoEvent);
+    });
+
+    it('submits the form', () => {
+        const pseudoEvent = {};
+
+        component.root.findByType('form').props.onSubmit(pseudoEvent);
+
+        expect(searchFormProps.onSearchSubmit).toHaveBeenCalledTimes(1);
+        expect(searchFormProps.onSearchSubmit).toHaveBeenCalledWith(pseudoEvent);
+    });
+
+    it('disables teh button and prevents submit', () => {
+       component.update(<SearchForm {...searchFormProps} searchTerm='' /> );
+
+       expect(component.root.findByType('button').props.disabled).toBeTruthy();
+    });
+});
