02880a345e526cea522058fac3bfa429e8ea42d1
Joseph Edwin Gill <josephedwingill@gmail.com>
2020-10-15 13:34:34 +0200

    Added additional App Jest tests, as well as snapshot for Item

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import axios from 'axios';
 import App, {Item, List, SearchForm, InputWithLabel} from "./App";
+
+jest.mock('axios');
 
 describe('Item', () => {
     const item = {
@@ -17,27 +20,32 @@ describe('Item', () => {
     let component;
 
     beforeEach(() => {
-        component = renderer.create(<Item item={item} onRemoveItem={handleRemoveItem} /> );
+        component = renderer.create(<Item item={item} onRemoveItem={handleRemoveItem}/>);
     });
 
     it('renders all properties', () => {
         expect(component.root.findByType('a').props.href).toEqual('https://reactjs.org/');
         expect(component.root.findAllByType('span')[1].props.children).toEqual('Jordan Walke');
-        expect(component.root.findAllByProps({ children: 'Jordan Walke'}).length).toEqual(2);
-        expect(component.root.findAllByProps({ children: 3}).length).toEqual(2);
-        expect(component.root.findAllByProps({ children: 4}).length).toEqual(2);
+        expect(component.root.findAllByProps({children: 'Jordan Walke'}).length).toEqual(2);
+        expect(component.root.findAllByProps({children: 3}).length).toEqual(2);
+        expect(component.root.findAllByProps({children: 4}).length).toEqual(2);
     });
 
     it('calls onRemoveItem on button click', () => {
-       component.root.findByType('button').props.onClick();
-       expect(handleRemoveItem).toHaveBeenCalledTimes(1);
-       expect(handleRemoveItem).toHaveBeenCalledWith(item);
-       expect(component.root.findAllByType(Item).length).toEqual(1);
+        component.root.findByType('button').props.onClick();
+        expect(handleRemoveItem).toHaveBeenCalledTimes(1);
+        expect(handleRemoveItem).toHaveBeenCalledWith(item);
+        expect(component.root.findAllByType(Item).length).toEqual(1);
     });
+
+    test('renders snapshot', () => {
+        let tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    })
 
 });
 
-describe ('List', () => {
+describe('List', () => {
     const list = [
         {
             title: 'React',
@@ -58,7 +66,7 @@ describe ('List', () => {
     ];
 
     it('renders two items', () => {
-        const component = renderer. create(<List list={list} />);
+        const component = renderer.create(<List list={list}/>);
         expect(component.root.findAllByType(Item).length).toEqual(2);
     });
 });
@@ -77,12 +85,12 @@ describe('SearchForm', () => {
     });
 
     it('renders the input field with its value', () => {
-       const value = component.root.findByType('input').props.value;
-       expect(value).toEqual('React');
+        const value = component.root.findByType('input').props.value;
+        expect(value).toEqual('React');
     });
 
     it('changes the input field', () => {
-        const pseudoEvent = { target: 'Redux' };
+        const pseudoEvent = {target: 'Redux'};
 
         component.root.findByType('input').props.onChange(pseudoEvent);
 
@@ -100,8 +108,61 @@ describe('SearchForm', () => {
     });
 
     it('disables teh button and prevents submit', () => {
-       component.update(<SearchForm {...searchFormProps} searchTerm='' /> );
+        component.update(<SearchForm {...searchFormProps} searchTerm=''/>);
 
-       expect(component.root.findByType('button').props.disabled).toBeTruthy();
+        expect(component.root.findByType('button').props.disabled).toBeTruthy();
+    });
+});
+
+describe('App', () => {
+    it('succeeds fetching data with a list', async () => {
+        const list = [
+            {
+                title: 'React',
+                url: 'https://reactjs.org/',
+                author: 'Jordan Walke',
+                num_comments: 3,
+                points: 4,
+                objectID: 0
+            },
+            {
+                title: 'Redux',
+                url: 'https://redux.js.org/',
+                author: 'Dan Abramov, Andrew Clark',
+                num_comments: 2,
+                points: 5,
+                objectID: 1,
+            },
+        ];
+
+        const promise = Promise.resolve({
+            data: {
+                hits: list,
+            }
+        });
+
+        axios.get.mockImplementationOnce(() => promise);
+
+        let component;
+
+        await renderer.act(async () => {
+            component = renderer.create(<App />);
+        })
+
+        expect(component.root.findByType(List).props.list).toEqual(list);
+    });
+
+    it('fails fetching data with a list', async () => {
+        const promise = Promise.reject();
+
+        axios.get.mockImplementationOnce(() => promise);
+
+        let component;
+
+        await renderer.act(async () => {
+            component = renderer.create(<App />);
+        })
+
+        expect(component.root.findByType('p').props.children).toEqual('Something went wrong ...');
     });
 });
